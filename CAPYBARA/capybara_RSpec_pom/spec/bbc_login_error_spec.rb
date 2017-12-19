e3f0f1c72e3beadec78851b23a35098130c6cbb8
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-12-19 15:39:24 +0000

    login successful test done and passed

--- CAPYBARA/capybara_RSpec_pom/spec/bbc_login_error_spec.rb
+++ CAPYBARA/capybara_RSpec_pom/spec/bbc_login_error_spec.rb
@@ -4,7 +4,7 @@ describe 'Incorrect user details produces valid error' do
 
   context 'it should respond with the correct error when incorrect details are input' do
 
-    it 'should produce an error when inputting an incorrect password to a valid account' do
+    it 'should produce an error when inputting an incorrect username and a valid password' do
       @bbc_site = BbcSite.new
       @bbc_site.bbc_homepage.visit_home_page
       @bbc_site.bbc_homepage.click_sign_in_link
@@ -12,10 +12,6 @@ describe 'Incorrect user details produces valid error' do
       @bbc_site.bbc_sign_in_page.fill_in_password('1234abcde')
       @bbc_site.bbc_sign_in_page.click_sign_in_button
       expect(@bbc_site.bbc_sign_in_page.incorrect_password_text).to eq("Sorry, we can’t find an account with that username. If you\'re over 13, try your email address instead or get help here.")
-
-      sleep 5
     end
-
   end
-
 end
--- CAPYBARA/capybara_RSpec_pom/spec/bbc_signup_failure_spec.rb
+++ CAPYBARA/capybara_RSpec_pom/spec/bbc_signup_failure_spec.rb
@@ -8,12 +8,11 @@ describe 'Correct user details produces successful login' do
       @bbc_site = BbcSite.new
       @bbc_site.bbc_homepage.visit_home_page
       @bbc_site.bbc_homepage.click_sign_in_link
-      @bbc_site.bbc_sign_in_page.fill_in_username('coding_sue@icloud.com')
+      @bbc_site.bbc_sign_in_page.click_register_link
+
       @bbc_site.bbc_sign_in_page.fill_in_password('Testing123')
       @bbc_site.bbc_sign_in_page.click_sign_in_button
       sleep 5
     end
-
   end
-
 end
